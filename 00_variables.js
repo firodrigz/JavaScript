aee9dce405968f79c888ffcfb3e1dc72a3f87418
firodrigz <firodrigz@gmail.com>
2023-11-07 09:32:41 -0300

    Tipos de variables y de datos

---
+++ 00_variables.js
@@ -0,0 +1,27 @@
+//VARIABLES
+
+//Tipo de variables let, var y const
+var global = "de uso global";
+let local = "de uso local + recomendada";
+const VALOR_CONSTANTE = 13.14;  
+
+//Objetos globales
+window // En navegadores
+global // En nodejs
+
+//Tipos de datos
+
+//Primitivos (accedemos al valor directamente)
+let string = "Facu";  // Los strings tiene la propiedad string.length y diversos métodos como por ejemplo concat()
+let number = 20; 
+let boolean = true;  //Puede ser true o false
+let nulo = null;
+let indefinido = undefined;
+
+//No primitivos - Compuestos (se accede por referencia) USO CONST
+const array = [1, "Facu", false]; //Puede contener cualquier tipo de dato
+const objeto = {
+    color:"transparent",
+    volume:1,
+    weight:0.5
+};
