c35d2b5d274675426077d489ee8385fe242c04a2
firodrigz <firodrigz@gmail.com>
2023-11-07 09:34:46 -0300

    Operadores y uso de backticks al concatenar

---
+++ 01_operadores_basicos.js
@@ -0,0 +1,48 @@
+//OPERADORES BÁSICOS
+
+//Suma-Concatena
+let suma = 10 + 20;
+console.log(suma);
+
+let name1="Pepe";
+let name2="Martin";
+let name3="Leonel";
+console.log("Hola" + " " + name1 + " " + name2 + " " + name3); // Concatena strings
+
+// Uso de backticks
+console.log(`Hola ${name1} ${name2} ${name3} - con backticks`);
+
+
+54 - 85; // Resta
+45 * 2; // Multiplicación
+9 / 3; // División entera sin decimal
+9 % 3; // Resto de la división
+9 < 10; // 9 menor que 10
+9 <= 10; // 9 menor igual que 10
+10 > 9; // 10 mayor que 9
+10 >= 9; // 10 mayor igual que 9
+3 ** 3 // 3 exponente 3 = 27
+
+//  (condicion) && (condicion)  And (Y)  solo es verdadero cuando las 2 condiciones son verdaderas
+//  (condicion) || (condicion) Or (O) 
+
+let variable = 'Bob'; 
+// Asignación
+let nro = 40;
+let rej, cont = 0;
+
+// Identidad/igualdad
+let miVariable = 3;
+miVariable == 4; // Comprueba si el valor es igual
+miVariable === 4;  //Comprueba si el valor y tipo de dato son igual , y devuelve un valor de true/false (booleano).
+
+miVariable !== 3; // Distinto
+! miVariable;  //Negador ¬
+
+/* PARA MÁS OPERADORES VISITAR: 
+
+https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators
+
+*/
+
+
