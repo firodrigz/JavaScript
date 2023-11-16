19017818a3421bd280450822a71d9da24f404696
firodrigz <firodrigz@gmail.com>
2023-11-16 20:52:14 -0300

    Ciclo FOR - WHILE - DO WHILE - CONTINUE/BREAK

--- 03_iteracion.js
+++ 03_iteracion.js
@@ -1,49 +1,66 @@
 //CICLOS - BUCLES
-
 //Se sale del ciclo solo cuando la condifición es falsa , si es verdadera queda en el ciclo!!
-
 
 // ITERACIÓN DEFINIDA (Ciclo For)
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];    
+let arreglo = [1, 4, 3, 5];
+let i;
+for (i = 0; i < arreglo.length; i++) {
+  console.log(arreglo[i]);
 }
 
-/*
-for IN muestra posición del arreglo
-for OF directamente muestra el contenido de la posición
-*/
+/* for IN muestra posición del arreglo
+for OF directamente muestra el contenido de la posición */
 
-let array1 = ["maría","josefa","roberta"];
-let array2 = ["pedro","marcelo",array1,"josefina"];
+let array1 = ["maría", "josefa", "roberta"];
+let array2 = ["pedro", "marcelo", array1, "josefina"];
 
-for(let array in array2){
-    if (array == 2) {
-        for(let array of array1){
-            console.log(array + "<br>");
-        }        
-    } else {
-        console.log(array2[array] + "<br>");        
+for (let array in array2) {
+  if (array == 2) {
+    for (let array of array1) {
+      console.log(array);
     }
+  } else {
+    console.log(array2[array]);
+  }
 }
 
-// Salida pedro marcelo maría josefa roberta
+// Salida pedro marcelo maría josefa roberta josefina
 
+/*DIFERENCIA ENTRE FOREACH Y MAP
+-forEach solo recorre elementos del arreglo y no retorna nada
+-Map reccorre elementos del arreglo Y retorna un nuevo arreglo MODIFICADO*/
+
+/* Uso del forEach */
+arreglo.forEach(function (element) {
+  console.log(element);
+}); //recorrido con forEach
+let lenguajes = ["ruby", "php", "c"];
+lenguajes.forEach(function (lenguaje, indice, arreglo) {
+  console.log(lenguaje, indice, arreglo);
+});
+
+/* Modificar arreglos con map */
+//genera un nuevo arreglo
+
+let numeros = [1, 2, 3, 5, 34];
+let cuadrados = numeros.map(function (numero) {
+  return numero * numero;
+});
+console.log(cuadrados);
 
 //ITERACIÓN CONDICIONADA (Ciclo While)
 
-let condition = 1;   // se debe ingresar la condición afuera
+let condition = 1; // se debe ingresar la condición afuera
 
 while (condition) {
-    condition++; //suma contador
-    let condition; //se vuelve a ingresar confición
+  condition++; //suma contador
+  let condition; //se vuelve a ingresar confición
 }
 
 // Puede pasar 0 o n veces por el while
 
-do {
-    
-} while (condition);
+do {} while (condition);
 
 //Pasa si o si una vez por el while
 
@@ -51,16 +68,16 @@ do {
 //continue se usa para saltar a la siguiente iteración del bucle y break para salir del bucle.
 
 for (let i = 0; i < 10; i++) {
-    const esPar = i % 2 === 0
-    if (esPar) {
-      continue
-    }
-  
-    // Solo mostramos este consola.log si es impar
-    console.log(i)
-  
-    // Salimos del bucle al llegar al 7
-    if (i === 7) {
-      break
-    }
+  const esPar = i % 2 === 0;
+  if (esPar) {
+    continue;
   }
+
+  // Solo mostramos este consola.log si es impar
+  console.log(i);
+
+  // Salimos del bucle al llegar al 7
+  if (i === 7) {
+    break;
+  }
+}
