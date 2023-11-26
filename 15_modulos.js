134188f07f63178913fcc18520a4bedf7927ed69
firodrigz <firodrigz@gmail.com>
2023-11-25 21:23:12 -0300

    Concepto de módulo

---
+++ 15_modulos.js
@@ -0,0 +1,9 @@
+// Es IMPORTANTE poner type="module" en la etiqueta script del html
+// Para que soporte versiones menores a ecmascript 6, utilizamos nomodule en la etiquieta script del html
+
+//import { } from ./archiv.js // para importar
+
+//export funcion, variable, etc
+
+//Las funciones flecha no se pueden usar en export default
+//con const y let no puedo hacer un export default en la declaración, primero declararlas, luego exportarlas por default
