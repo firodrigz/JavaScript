82de17ac3335635097b440dd4ab493880db8d09b
firodrigz <firodrigz@gmail.com>
2023-12-24 16:37:16 -0300

    Promesas como solución al callback hell

--- 18_promesas.js
+++ 18_promesas.js
@@ -1,46 +1,53 @@
 //Promesas
 
 function cuadradoPromise(value) {
-  if (typeof value !== "number") {
+  /*if (typeof value !== "number") {
     return Promise.reject({
       msg: `Error, el valor ${value} ingresado, no es un nro`,
     });
-  }
-  
+  }*/
+
   return new Promise((resolve, reject) => {
+    if (typeof value !== "number") {
+      reject({
+        msg: `Error, el valor ${value} ingresado, no es un nro`,
+      });
+    }
+
     setTimeout(() => {
       resolve({
         value,
         result: value * value,
       });
     }, 0 | (Math.random() * 1000));
+
   });
 }
 
 cuadradoPromise(0)
-  .then((obj) => {
-    //console.log(obj);
-    console.log(`Promise: ${obj.value}, ${obj.result}`);
+  .then((response) => {
+    //console.log(response);
+    console.log(`Promise: ${response.value}, ${response.result}`);
     return cuadradoPromise(1);
   })
-  .then((obj) => {
-    console.log(`Promise: ${obj.value}, ${obj.result}`);
+  .then((response) => {
+    console.log(`Promise: ${response.value}, ${response.result}`);
     return cuadradoPromise(2);
   })
-  .then((obj) => {
-    console.log(`Promise: ${obj.value}, ${obj.result}`);
+  .then((response) => {
+    console.log(`Promise: ${response.value}, ${response.result}`);
     return cuadradoPromise(3);
   })
-  .then((obj) => {
-    console.log(`Promise: ${obj.value}, ${obj.result}`);
+  .then((response) => {
+    console.log(`Promise: ${response.value}, ${response.result}`);
     return cuadradoPromise(4);
   })
-  .then((obj) => {
-    console.log(`Promise: ${obj.value}, ${obj.result}`);
+  .then((response) => {
+    console.log(`Promise: ${response.value}, ${response.result}`);
     return cuadradoPromise(5);
   })
-  .then((obj) => {
-    console.log(`Promise: ${obj.value}, ${obj.result}`);
+  .then((response) => {
+    console.log(`Promise: ${response.value}, ${response.result}`);
     console.log("Fin Promise");
   })
   .catch((err) => console.error(err));
